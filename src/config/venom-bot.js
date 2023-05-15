0b4debf9c030d5aebb35d847909b6923dfeacf19
JoaoAlvesC <jooao.v@outlook.com>
2023-05-15 15:08:32 -0300

    teste

--- src/config/venom-bot.js
+++ src/config/venom-bot.js
@@ -1,20 +1,39 @@
 import { create } from 'venom-bot';
 
 const chromiumArgs = [
-    '--disable-web-security', '--no-sandbox', '--disable-web-security',
-    '--aggressive-cache-discard', '--disable-cache', '--disable-application-cache',
-    '--disable-offline-load-stale-cache', '--disk-cache-size=0',
-    '--disable-background-networking', '--disable-default-apps', '--disable-extensions',
-    '--disable-sync', '--disable-translate', '--hide-scrollbars', '--metrics-recording-only',
-    '--mute-audio', '--no-first-run', '--safebrowsing-disable-auto-update',
-    '--ignore-certificate-errors', '--ignore-ssl-errors', '--ignore-certificate-errors-spki-list'
+    '--disable-web-security',
+    '--no-sandbox',
+    '--disable-web-security',
+    '--aggressive-cache-discard',
+    '--disable-cache',
+    '--disable-application-cache',
+    '--disable-offline-load-stale-cache',
+    '--disk-cache-size=0',
+    '--disable-background-networking',
+    '--disable-default-apps',
+    '--disable-extensions',
+    '--disable-sync',
+    '--disable-translate',
+    '--hide-scrollbars',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-first-run',
+    '--safebrowsing-disable-auto-update',
+    '--ignore-certificate-errors',
+    '--ignore-ssl-errors',
+    '--ignore-certificate-errors-spki-list',
 ];
 
 export const botInit = async ({ session }) => {
-    const client = await create({
-        browserArgs: chromiumArgs,
+    const client = await create(
         session,
-    });
+        (base64Qrimg, asciiQR, attempts) => {
+            console.log('Number of attempts to read the qrcode: ', attempts);
+            console.log('Terminal qrcode: ', asciiQR);
+            console.log('base64 image string qrcode: ', base64Qrimg);
+        },
+        { useChrome: false, browserArgs: ['--no-sandbox'] }
+    );
     console.log('Bot initialized successfully');
     return client;
 };
